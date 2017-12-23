712ce58513050480eddd7f4714cca351e7b0c0ac
eagobert <eagobert@hotmail.com>
2017-12-23 17:46:22 -0500

    commit additional changes

--- src/pages/add-inventory/add-inventory.ts
+++ src/pages/add-inventory/add-inventory.ts
@@ -81,7 +81,8 @@ export class AddInventoryPage {
         targetWidth: 500,
         destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
+        mediaType: this.camera.MediaType.PICTURE,
+        saveToPhotoAlbum: true
     
       }
 
